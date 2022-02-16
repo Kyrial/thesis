554960679cd82cf7af92210d1b1a3a24ed0674b0
Melvin <melvin.bardin1998@gmail.com>
2022-02-16 10:06:42 +0100

    mise a jour mineur

--- code/pre_trained_models/main_pca.py
+++ code/pre_trained_models/main_pca.py
@@ -31,8 +31,8 @@ import sparsenesslib.high_level as hl
 PIL.Image.MAX_IMAGE_PIXELS = 30001515195151997
 478940                             
 #'CFD','SCUT-FBP','MART','JEN','SMALLTEST','BIGTEST'
-#list_bdd = ['CFD','MART','JEN','SCUT-FBP','SMALLTEST','BIGTEST'] #['BIGTEST'] #"
-list_bdd = ['SMALLTEST'] 
+list_bdd = ['CFD','MART','JEN','SCUT-FBP','SMALLTEST','BIGTEST'] #['BIGTEST'] #"
+#list_bdd = ['SMALLTEST'] 
 model_name = 'VGG16'  # 'vgg16, resnet (...)'
 #weights = 'vggface' #'imagenet','vggface'
 list_weights = ['imagenet'] #['vggface','imagenet','vggplace']
